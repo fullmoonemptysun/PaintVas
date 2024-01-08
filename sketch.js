7a66cb55df6ea6af98337648400f039d24b366ea
moon <kadiradaliali@gmail.com>
2024-01-07 19:57:42 -0700

    Add styles, layout, theme functionality, favicon and toolbox (not functional).

--- sketch.js
+++ sketch.js
@@ -1,26 +1,64 @@
+//Starting variables
+const colorRadioButton = document.querySelector("#color");
+const textureRadioButton = document.querySelector("#texture");
+const rainbowRadioButton = document.querySelector("#rainbow");
+const darkenRadioButton = document.querySelector("#darken");
+const eraserRadioButton = document.querySelector("#eraser");
+const colorpicker = document.querySelector(".colorpicker");
 const container = document.querySelector('#container');
+const num = Number(prompt("n = ?"));
+const bod = document.querySelector("body");
+const sunButton = document.querySelector(".light");
+const moonButton = document.querySelector(".dark");
+
+
+//Event Listeners for pen buttons
+
+
+
+bod.classList.add("lightTheme");
+//Event Listeners for theme buttons
+sunButton.addEventListener("click", function(e){
+    bod.classList.remove("lightTheme");
+    sunButton.classList.toggle("invisible");
+    moonButton.classList.toggle("invisible");
+    bod.classList.add("darkTheme");
+    bod.classList.toggle("lightFont")
+})
+moonButton.addEventListener("click", function(){
+    bod.classList.remove("darkTheme");
+    moonButton.classList.toggle("invisible");
+    sunButton.classList.toggle("invisible");
+    bod.classList.add("lightTheme");
+    bod.classList.toggle("lightFont");
+})
+
+
 
 //to detect when the mouse is held down
 let mouseDown = false;
 document.addEventListener('mousedown', function(e){
     mouseDown = true;
 });
-
 document.addEventListener('mouseup', function(e){
     mouseDown = false;
 });
 
+
+
+//grid starting variables
 let stylesheet = getComputedStyle(container);
-let height = (stylesheet.getPropertyValue('height'));
-let width = (stylesheet.getPropertyValue('width'));
+let heightString = (stylesheet.getPropertyValue('height'));
+let widthString = (stylesheet.getPropertyValue('width'));
+let height = (heightString.match(/\d+/))[0];
+let width = (widthString.match(/\d+/))[0];
 
-let cellHeight = (height/100);
-let cellWidth = (height/100);
 
 
-//detect if the left button is pressed
-for(let i = 0; i < 64; i++){
-    for(let j = 0; j < 64; j++){
+
+function createGrid(num){
+for(let i = 0; i < num; i++){
+    for(let j = 0; j < num; j++){
         const gridBlock = document.createElement('div');
         gridBlock.classList.add('gridCell');
         gridBlock.addEventListener('mousedown', function(e){
@@ -31,13 +69,16 @@ for(let i = 0; i < 64; i++){
                gridBlock.classList.add('activeCell') 
             }
         })
-
         
-
-        
-        gridBlock.style.cssText = "height:calc(640px/64); width:calc(640px/64);";
+        gridBlock.style.height = `calc(100%/${num})`;
+        gridBlock.style.width = `calc(100%/${num})`;
+        gridBlock.style.userSelect = "none";
         container.appendChild(gridBlock);
 
     }
 }
+}
 
+
+createGrid(num);
+const cells = document.querySelectorAll(".gridCell");
